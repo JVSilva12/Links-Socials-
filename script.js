1387a0092c0af4548104661163a23a8f51c35adb
JVSilva12 <joao.vitor.silva61@aluno.ifec.gov.br>
2022-07-08 19:27:00 -0300

    Atualização 08.07.2022/1.2

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var data = new Date();
 var hora = data.getHours();
 var dark = document.querySelector('.body');
 
-if (hora < 6 || data >= 18){
+if (hora < 6 || hora >= 18){
     dark.classList.toggle('dark');
 }
